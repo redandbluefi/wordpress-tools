11a0b3157d3b3388695fdd3009074771bc619c65
Christian Nikkanen <hi@kisu.li>
2017-05-05 11:37:11 +0300

    Add env helpers

--- modules/core.php
+++ modules/core.php
@@ -20,6 +20,24 @@ function register_strings() {
   }
 }
 
+function env() {
+  if (defined('WP_ENV')) {
+    return WP_ENV;
+  } else {
+    define('WP_ENV', getenv('WP_ENV') ?? 'production');
+  }
+
+  return WP_ENV;
+}
+
+function is_prod() {
+  return env() === 'production';
+}
+
+function is_dev() {
+  return env() === 'development';
+}
+
 function tag($parts = [], $glue = ' ') {
   return \join($glue, $parts);
 }
